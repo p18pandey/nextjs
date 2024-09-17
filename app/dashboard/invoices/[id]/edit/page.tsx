3923220bfd0a67ab269305658671fec3bec0b432
p18pandey <118770054+p18pandey@users.noreply.github.com>
2024-09-17 13:56:20 +0000

    developing..2

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 
 
@@ -11,6 +12,9 @@ import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
       fetchInvoiceById(id),
       fetchCustomers(),
     ]);
+    if (!invoice) {
+      notFound();
+    }
 
   return (
     <main>
